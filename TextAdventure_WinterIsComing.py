0b31a263433e2e649fa30b335739661a3b216750
Schallbert <schal@mailbox.org>
2019-10-09 23:08:46 +0200

    Now working: Room list attached to rooms TODO next: Clarify concept of inventory (list or dict?), alongside define where objects should be generated (in rooms/spot class or rather in helper functions?

--- TextAdventure_WinterIsComing.py
+++ TextAdventure_WinterIsComing.py
@@ -47,16 +47,22 @@ def inputCheck(text, expNumOfDigits):
 def spotList(roomNumber):
     """Generates a list of spots that the roon contains
     based on room number, using spot dictionary."""
-    listField = 0
     spotObjList = {}
     #get valid spot numbers for room
     for i in range(roomNumber, roomNumber+9):
         if i in dictSpots:
             #generate spots
             spotObjList[i] = Spot(i)
-            listField = listField + 1
     return spotObjList
 
+def roomList(roomNumber):
+    roomObjList = {}
+    for i in range(0, len(dictConnectedRooms[roomNumber])):
+        #generate adjacent rooms
+        roomObjList[dictConnectedRooms[roomNumber][i]] = Room(dictConnectedRooms[roomNumber][i])
+    return roomObjList
+    
+#----------------------------------------------
 def itemBuilder(generateFromNr):
     """Generates the item connected to the spot, if any"""
     #check if called via spot (3-digit) number
@@ -120,16 +126,27 @@ class Room:
         self.number = number
         self.name = dictRooms[number]
         self.description = dictTexts[number]
-        self.spot_list = spotList(number)
 
     def onEnter(self):
-        print(self.description + "\n")
-        textReader("Ihr befindet euch " + self.name + ".\n\n")
+        dictRoomsInventory.append(self.number)
+        self.spot_list = spotList(self.number)
+        self.room_list = roomList(self.number)
+        textReader(self.description)
+        textReader("Ihr befindet euch bei " + str(self.number) + ": "\
+                   + self.name + ".\n\n")
         print("Ihr seht:")
-        
         for element in self.spot_list.values():
             print(str(element.number) + ": "\
                   + element.name)
+            time.sleep(.5)
+        print("\nVon hier aus sind folgende Orte erreichbar:")
+        for element in self.room_list.values():
+            if element.number < self.number:
+                #room is known
+                print(str(element.number) + ": "\
+                      + element.name)
+            else:
+                print(str(element.number) + ": ???")
             time.sleep(.5)
 
 class Spot:
@@ -197,42 +214,82 @@ class Player:
 
 #Text Dictionary by IDs:
 dictTexts = {
-    10 : "Euer Smartphone. Kann so ziemlich alles und ist natürlich auch mit Taschenlampe, \
-Kamera und Navigationssystem ausgestattet.\nHier draußen scheinen allerdings weder Mobilfunk\
-noch Datenvervindung möglich zu sein.\nZudem ist der Akkustand niedrig und ihr habt keine Powerbank dabei.",
-    11 : "Ein kaum leserliches Foto der Umgebungskare, aufgenommen mit dem Handy.\
+    10 : \
+"Euer Smartphone. Kann so ziemlich alles und ist natürlich auch mit Taschenlampe,\n\
+Kamera und Navigationssystem ausgestattet.\n\
+Hier draußen scheinen leider weder Mobilfunk noch Datenvervindung möglich zu sein.\n\
+Zudem ist der Akkustand niedrig und ihr habt keine Powerbank dabei.",
+    11 : \
+"Ein kaum leserliches Foto der Umgebungskare, aufgenommen mit dem Handy.\n\
 Die Spiegelung der hellen Hauswand gegenüber links des Kastens hingegen zeigt alle Details.\n",
-    12 : "Das Foto der Umgebungskarte.\nDie Karte ist scharf und detailliert zu erkennen.\n",
-    13 : "Ein Standard Auto-Verbandkasten.\nNicht ganz klar, ob das jetzt bei einem Tagesausflug \
-zu Fuß noch dem Credo 'Vorsicht ist die Mutter der Porzellankiste' \
-entspricht\noder glattweg in die Schublade 'Panisch und übertrieben vorsichtig' fällt...\n",
-    14 : "Wahnsinn, was Hunger im Einkaufsladen anrichten kann!\nEure Rucksäcke platzen fast vor \
-Zutaten für eine wahre Picknick-Orgie. Wenn ihr das alles esst, werdet ihr wohl kaum weiterwandern wollen ;)\n",
-    100 : "Eine Bahnstation in der tiefsten Eifel...\n",
-    101 : "Eine große, ziemlich detaillierte Karte der Umgebung in einem etwas ramponiert \
-aussehendem Glaskasten.\nDie Sonne spiegelt sich so sehr darin, dass man kaum etwas erkennen kann.\n\
+    12 : \
+"Das Foto der Umgebungskarte.\n\
+Die Karte ist scharf und detailliert zu erkennen.\n",
+    13 : \
+"Ein Standard Auto-Verbandkasten.\n\
+Nicht ganz klar, ob das jetzt bei einem Tagesausflug zu Fuß noch dem Credo\n\
+'Vorsicht ist die Mutter der Porzellankiste' entspricht oder glattweg\n\
+in die Schublade 'Panisch und übertrieben vorsichtig' fällt...\n",
+    14 : \
+"Wahnsinn, was Hunger im Einkaufsladen anrichten kann!\n\
+Eure Rucksäcke platzen fast vor Zutaten für eine wahre Picknick-Orgie.\n\
+Wenn ihr das alles esst, werdet ihr wohl kaum weiterwandern wollen ;)\n",
+    100 : \
+"Eine Bahnstation in der tiefsten Eifel...\n",
+    101 : \
+"Eine große, ziemlich detaillierte Karte der Umgebung in einem etwas ramponiert\n\
+aussehendem Glaskasten.\n\
+Die Sonne spiegelt sich so sehr darin, dass man kaum etwas erkennen kann.\n\
 Ihr Titel: 'Die V_____eifel: Mend__ ___ ____bung, 1:10000'\n",
-    102 : "Der linke Pfosten des Glaskastens.\nEr trägt zwar Rostspuren, \
+    102 : \
+"Der linke Pfosten des Glaskastens.\nEr trägt zwar Rostspuren,\n\
 scheint aber grundsolide und tief im Boden verankert.\n",
-    103 : "Die 'Adler-Apotheke' in Mendig. Kennste eine, kennste alle...\n",
-    104 : "Ein REWE-Markt, ziemlich groß für diese Gegend.\n\
+    103 : \
+"Die 'Adler-Apotheke' in Mendig. Kennste eine, kennste alle...\n",
+    104 : \
+"Ein REWE-Markt, ziemlich groß für diese Gegend.\n\
 Der hat bestimmt alles, was man vergessen haben könnte.\n",
-    105 : "Die große Karte der Umgebung ist jetzt verschattet, da eine von euch vor dem Pfosten steht.\
-Man kann die Karte jetzt klar und deutlich erkennen.\n"
+    105 : \
+"Die große Karte der Umgebung ist jetzt verschattet, da jemand von euch\n\
+vor dem Pfosten steht. Man kann die Karte jetzt klar und deutlich erkennen.\n",
+    110 : \
+"Der Fußweg zu eurem ersten Etappenziel Maria Laach.\n\
+Ihr wolltet ungern an der Landstraße entlang gehen und habt euch für diesen\n\
+zugewachsenen, schmalen Weg entschieden,\n\
+den das Wild sicher auch als Straße benutzt. Die Sonne blinzelt immer wieder\n\
+durch das Blätterdach, es duftet nach Heckenrosen und ab und zu erhascht ihr\n\
+einen Blick auf den Wald.\n\
+Schließlich gelangt ihr an eine Weggabelung in einer kleinen Senke.\n\
+Der Wegweiser wird den letzten Winter wohl nicht überlebt haben,\n\
+sein mit zackigen Holzsplittern versehener Stumpf ragt\n\
+ein paar Zentimeter aus dem Boden.\n",
+    120: \
+"Nach ein paar hundert Metern sanft bergab verläuft sich der Weg\n\
+und endet in lockeren, aber leicht erhöhten, festen Grasbüscheln.\n\
+Die Natur hat euch hier förmlich verschluckt.\n\
+Doch - halt - was glitzert da hinten im Gras?\n",
+    130:
+"Hier geht es eindeutig bergauf. Nicht, dass ihr das am Gelände sehen könnt,\n\
+nein, dafür ist das Unterholz viel zu dicht, aber es ist irgendwie...\n\
+anstrengend und wird eher noch anstrengender. \n\
+Vorne seht ihr eine Lichtung, vielleicht gut für eine Rast?\n"
     }
 
 #Defines room number and names
 dictRooms = {
     100 : "Bahnhof Mendig",\
-    110 : "Fußweg Maria Laach"\
+    110 : "Fußweg Maria Laach",\
+    120 : "Sumpf im Wald",\
+    130 : "Fuß des Hügels vor dem Bergkamm",\
+    140 : "Wiese auf dem Bergkamm"\
     }
 
 #Defines which rooms are in what way connected to which rooms
 dictConnectedRooms = {
     100 : [110],\
-    110 : [100, 120],\
-    120 : [110, 130, 140],\
-    130 : [120],\
+    110 : [100, 120, 130],\
+    120 : [110],\
+    130 : [110, 140],\
     #...
     }
 
@@ -281,7 +338,8 @@ dictSpotActions = {
     105 : Action_id.VIEW\
     }
 
-dictInventory = {}
+dictItemsInventory = []
+dictRoomsInventory= []
 
 #Vorgeschichte!
 #Room1: Train station Mendig, RB26, 1.5h from cologne     
@@ -291,9 +349,20 @@ currentRoom = Room(100)
 #print(player1.name)
 #print(player1.fitness)
 
+#enter a room routine
 currentRoom.onEnter()
-activeSpot = currentRoom.spot_list[inputCheck("Was wollt ihr untersuchen? ", 3)]
-activeSpot.action()
+playerAction = inputCheck("Was wollt ihr untersuchen? ", 3)
+print("\n")
+if playerAction in dictRooms:
+    #player selected a room
+    currentRoom = currentRoom.room_list[playerAction]
+    currentRoom.onEnter()
+elif playerAction in dictSpots:
+    #player selected a spot
+    activeSpot = currentRoom.spot_list[playerAction]
+else:
+    #player selected an item, an item-item combination, or an item-spot combination
+    print("TODO: as per comment above")
 
 
 
