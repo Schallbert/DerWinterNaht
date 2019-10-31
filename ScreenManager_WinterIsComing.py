58c64d8c8368e707c81e81b076c53dca3b22acdc
Schallbert <schal@mailbox.org>
2019-10-31 22:51:27 +0100

    Some more or less ugly changes (textReader update...) Added color change to GUI. Need to correctly serialize events.

--- ScreenManager_WinterIsComing.py
+++ ScreenManager_WinterIsComing.py
@@ -10,6 +10,8 @@ Kamera und Navigationssystem ausgestattet.\n\
 Hier draußen scheinen leider weder Mobilfunk noch Datenvervindung möglich zu sein.\n\
 Zudem ist der Akkustand niedrig und ihr habt keine Powerbank dabei."
 
+
+
 # ------------------------------------
 # HELPERS
 def inputCheck(resp):
@@ -21,7 +23,31 @@ def inputCheck(resp):
             quitSave()
         else:
             print("Bitte eine Zahl eingeben oder das Spiel mittels 'quit' beenden.")
-            mycanvas.inputScreen.Activate()
+            gui.inputScreen.Activate()
+
+def textReader(widget, index, text):
+    if len(text) > 0:
+        widget.insert(index, text[0])
+        if len(text) > 1:
+            # compute index of next char
+             index = widget.index("%s + 1 char" % index)
+             # type next char [ms]
+             widget.after(30, textReader, widget, index, text[1:])
+             #add blip-like sound on output?
+        else:
+            widget.config(fg=GuiVars.dictCP["Y4"])
+            widget.config(bg=GuiVars.dictCP["B1"])
+            widget.config(state=DISABLED)
+
+def quitSave():
+    print("Speichern...")
+    save()
+    print("Spiel wird beendet.")
+    quit()
+
+def save():
+    print("IMPLEMENT SAVE!")
+    print("Gespeichert!")
 
 # ------------------------------------
 # CLASSES
@@ -48,27 +74,44 @@ class InputText(Text):
     #replace init function by "custom" init
     def __init__(self,parent,**kwargs):
         Text.__init__(self,parent,**kwargs)
+
+    def BindToKey(self, key):
         #bind self to event "return", calling inputCheck function
         #handing over its contents-last character (which is \n)
-        self.bind('<Return>', \
-                    lambda event, \
-                    : self.__EventHandler())
+        self.bind(key, \
+                  lambda event, \
+                  : self.__EventHandler())
 
     def __EventHandler(self):
         #get input, deactivate box, check input
         inpt = self.get("1.0", "end-1c")
         self.__Deactivate() 
-        inputCheck(inpt)     
+        inputCheck(inpt)
+
+    def Update(self, text):
+        self.config(state=NORMAL)
+        self.delete('2.0', END)
+        self.insert(INSERT, text)
+        self.config(state=DISABLED)
+
+    def Write(self, text):
+        self.Activate()
+        textReader(self, '1.0', text)
         
     def Activate(self):
         self.config(state=NORMAL)
+        self.delete('1.0', END)
+        self.config(fg=GuiVars.dictCP["Y5"])
+        self.config(bg=GuiVars.dictCP["B2"])
         self.focus_set()
         
     def __Deactivate(self):
         #clear text then disable text screen
-        self.delete('1.0', END)
+        self.config(fg=GuiVars.dictCP["B2"])
+        self.config(bg=GuiVars.dictCP["B1"])
         self.config(state=DISABLED)
 
+   
 #variables needed for gui setup
 class GuiVars:
     #Color Palette used throughout the game
@@ -98,52 +141,64 @@ class GuiVars:
         self.scr_w = w-self.__xWMgn
         self.scr_h = h-self.__yWMgn
 
-def SetupGui():
-    root = Tk() #instantiate TKINTER (Gui package)
-    var = GuiVars() #variable container for gui setup
-    root.title("Der Winter Naht")
-    var.SetScrSize(int(root.winfo_screenwidth()), int(root.winfo_screenheight()))
-
-    img = PhotoImage(file='Noise.gif') #TEST, to be replaced with game title image
-
-    #Canvas
-    myframe = Frame(root)
-    myframe.pack(fill=BOTH, expand=YES)
-    mycanvas = ResizingCanvas(myframe,width=var.scr_w, height=var.scr_w, bg="black")
-    mycanvas.pack(fill=BOTH, expand=YES)
-    #Frames
-    mycanvas.textScrFr = Frame(mycanvas, bg=var.frClr, bd=var.frBdr)
-    mycanvas.gameScrFr = Frame(mycanvas.textScrFr, bg="black", bd=var.frBdr)
-    mycanvas.invtScrFr = Frame(mycanvas, bg=var.frClr, bd=var.frBdr)
-    mycanvas.statsScrFr = Frame(mycanvas, bg=var.frClr, bd=var.frBdr)
-    mycanvas.inputScrFr = Frame(mycanvas, bg=var.frClr, bd=var.frBdr)
-
-    mycanvas.textScrFr.pack(anchor=NW, fill=BOTH, expand=YES)
-    mycanvas.gameScrFr.pack(side=RIGHT, fill=BOTH, expand=YES)
-    mycanvas.invtScrFr.pack(anchor=S, side=LEFT, fill=X, expand=YES)
-    mycanvas.statsScrFr.pack(anchor=S, side=LEFT, fill=X, expand=YES)
-    mycanvas.inputScrFr.pack(anchor=S, side=LEFT, fill=X, expand=YES)
-
-    #Screens   
-    mycanvas.textScreen = Text(mycanvas.textScrFr, bg=var.scrBg, fg=var.scrFg, width=83, height=20, padx=12, pady=9)  
-    mycanvas.gameScreen = Label(mycanvas.gameScrFr, image=img, width=var.scr_w/2, height=var.scr_w*(9/32))
-    mycanvas.inventoryScreen = Text(mycanvas.invtScrFr, bg=var.scrBg, fg=var.scrFg, width=60, height=10, padx=12, pady=9)
-    mycanvas.statsScreen = Text(mycanvas.statsScrFr, bg=var.scrBg, fg=var.scrFg, width=40, height=10, padx=12, pady=9)
-    mycanvas.inputScreen = InputText(mycanvas.inputScrFr, font=("Helvetica", "63"), width=5, height=1, padx=24, bg=var.scrBg, fg=var.scrFg, pady=42)
+class GameGui:
     
-    mycanvas.textScreen.pack(side = LEFT, fill=Y, expand=YES)
-    mycanvas.gameScreen.pack(anchor = CENTER, expand=YES) 
-    mycanvas.inventoryScreen.pack(side = BOTTOM, fill=X, expand=YES) 
-    mycanvas.statsScreen.pack(side = LEFT, fill=X, expand=YES)  
-    mycanvas.inputScreen.pack(side = LEFT, fill=X, expand=YES)
+    def __init__(self):
+        root = Tk() #instantiate TKINTER (Gui package)
+        img = PhotoImage(file='Noise.gif') #TEST, to be replaced with game title image
+        var = GuiVars() #variable container for gui setup
+        root.title("Der Winter Naht")
+        var.SetScrSize(int(root.winfo_screenwidth()), int(root.winfo_screenheight()))
 
-    #Texts
-    mycanvas.textScreen.insert(INSERT, textScreenText)
-    mycanvas.inputScreen.Activate()
-    mycanvas.inventoryScreen.insert(INSERT, "Inventar")
-    mycanvas.statsScreen.insert(INSERT, "Name          Motivation     Müdigkeit")
+        #Canvas
+        myframe = Frame(root)
+        myframe.pack(fill=BOTH, expand=YES)
+        canvas = ResizingCanvas(myframe,width=var.scr_w, height=var.scr_w, bg="black")
+        canvas.pack(fill=BOTH, expand=YES)
+        #Frames
+        self.textScrFr = Frame(canvas, bg=var.frClr, bd=var.frBdr)
+        self.gameScrFr = Frame(self.textScrFr, bg="black", bd=var.frBdr)
+        self.invtScrFr = Frame(canvas, bg=var.frClr, bd=var.frBdr)
+        self.statsScrFr = Frame(canvas, bg=var.frClr, bd=var.frBdr)
+        self.inputScrFr = Frame(canvas, bg=var.frClr, bd=var.frBdr)
 
+        self.textScrFr.pack(anchor=NW, fill=BOTH, expand=YES)
+        self.gameScrFr.pack(side=RIGHT, fill=BOTH, expand=YES)
+        self.invtScrFr.pack(anchor=S, side=LEFT, fill=X, expand=YES)
+        self.statsScrFr.pack(anchor=S, side=LEFT, fill=X, expand=YES)
+        self.inputScrFr.pack(anchor=S, side=LEFT, fill=X, expand=YES)
 
+        #Screens   
+        self.textScreen = InputText(self.textScrFr, insertontime=0, bg=var.scrBg, fg=var.scrFg, width=83, height=20, padx=12, pady=9)  
+        self.gameScreen = Label(self.gameScrFr, image=img, width=var.scr_w/2, height=var.scr_w*(9/32))
+        self.inventoryScreen = InputText(self.invtScrFr, bg=var.scrBg, fg=var.scrFg, width=60, height=10, padx=12, pady=9)
+        self.statsScreen = InputText(self.statsScrFr, bg=var.scrBg, fg=var.scrFg, width=40, height=10, padx=12, pady=9)
+        self.inputScreen = InputText(self.inputScrFr, font=("Helvetica", "63") \
+                                         ,insertbackground=var.scrFg\
+                                         , insertofftime=1200\
+                                         , insertontime=1200\
+                                         , insertwidth=5\
+                                         , width=5\
+                                         , height=1\
+                                         , padx=24\
+                                         , bg=var.scrBg\
+                                         , fg=var.scrFg\
+                                         , pady=42)
+        
+        self.textScreen.pack(side = LEFT, fill=Y, expand=YES)
+        self.gameScreen.pack(anchor = CENTER, expand=YES) 
+        self.inventoryScreen.pack(side = BOTTOM, fill=X, expand=YES) 
+        self.statsScreen.pack(side = LEFT, fill=X, expand=YES)  
+        self.inputScreen.pack(side = LEFT, fill=X, expand=YES)
+
+        self.inputScreen.BindToKey('<Return>')
+        #Texts
+        self.inventoryScreen.insert(INSERT, "Inventar")
+        self.statsScreen.insert(INSERT, "Name          Motivation     Müdigkeit")
+
+gui = GameGui()
+gui.textScreen.Write(textScreenText)
+gui.inputScreen.Activate()
 
 # def main():
     # root.mainloop()
